c6991fd64b3163f7506b3ffedc53fa727ad9dac8
jhums04 <jbonganciso04@gmail.com>
2024-07-09 18:08:33 +0800

    initial commit

---
+++ src/main/java/com/springboot_mysql_dockerize/dockerize_springboot_with_mysql/DockerizeSpringbootWithMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_mysql_dockerize.dockerize_springboot_with_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeSpringbootWithMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeSpringbootWithMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_mysql_dockerize/dockerize_springboot_with_mysql/DockerizeSpringbootWithMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_mysql_dockerize.dockerize_springboot_with_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizeSpringbootWithMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
