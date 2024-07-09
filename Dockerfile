4122e054c4da3d04734c2b036b1c52201972112c
jhums04 <jbonganciso04@gmail.com>
2024-07-10 00:49:43 +0800

    Added docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,17 @@
+#FROM openjdk:17-jdk
+#WORKDIR /app
+#COPY target/dockerize-springboot-with-mysql-0.0.1-SNAPSHOT.jar /app/dockerize-springboot-mysql.jar
+#EXPOSE 8081
+#CMD ["java", "-jar", "dockerize-springboot-mysql.jar"]
+
 FROM openjdk:17-jdk
-WORKDIR /app
-COPY target/dockerize-springboot-with-mysql-0.0.1-SNAPSHOT.jar /app/springboot-app-with-mysql.jar
-EXPOSE 8081
-CMD ["java", "-jar", "dockerize-springboot-mysql.jar"]
+
+# Copy the JAR package into the image
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+
+# Expose the application port
+EXPOSE 8080
+
+# Run the App
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/com/springboot_mysql_dockerize/dockerize_springboot_with_mysql/DockerizeSpringbootWithMysqlApplication.java
+++ src/main/java/com/springboot_mysql_dockerize/dockerize_springboot_with_mysql/DockerizeSpringbootWithMysqlApplication.java
@@ -2,9 +2,17 @@ package com.springboot_mysql_dockerize.dockerize_springboot_with_mysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerizeSpringbootWithMysqlApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerizeSpringbootWithMysqlApplication.class, args);
